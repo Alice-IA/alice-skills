5292f1826aca272abe0d8089deee1070dbc3bca3
Alice-IA <yuiassistant@gmail.com>
2023-05-03 20:30:26 -0600

    add naptime skill

---
+++ skill-naptime/README.md
@@ -0,0 +1,31 @@
+# <img src='https://raw.githack.com/FortAwesome/Font-Awesome/master/svgs/solid/bed.svg' card_color='#22a7f0' width='50' height='50' style='vertical-align:bottom'/> Naptime
+Put Alice to sleep when you don't want to be disturbed
+
+## About 
+Tell Alice to sleep when you don't want to be disturbed in any way.
+This stops all calls to Speech to Text system, guaranteeing your voice won't
+be sent anywhere on an accidental activation.
+
+When sleeping, Alice will only listen locally for the phrase "Alice,
+wake up". Otherwise the system will be totally silent and won't bother you.
+
+On a Mark 1 this also dims the eyes.
+
+## Examples 
+* "Go to sleep"
+* "Nap time"
+* "Wake up"
+
+## Credits 
+Alice IA
+
+## Category
+**Daily**
+Configuration
+
+## Tags
+#nap
+#naptime
+#sleep
+#donotdisturb
+#do-not-disturb
---
+++ skill-naptime/__init__.py
@@ -0,0 +1,155 @@
+import time
+
+from alice import AdaptIntent, AliceSkill, intent_handler
+from alice.audio import wait_while_speaking
+from alice.configuration.config import Configuration
+from alice.messagebus.message import Message
+
+
+class NapTimeSkill(AliceSkill):
+    """Skill to handle alice speech client listener sleeping."""
+
+    def __init__(self) -> None:
+        """Initialize NapTimeSkill"""
+        super().__init__()
+        self.started_by_skill = False
+        self.sleeping = False
+        self.old_brightness = 30
+        self.disabled_confirm_listening = False
+
+    def initialize(self) -> None:
+        """Perform final initialization once Skill class has loaded."""
+        self.add_event('alice.awoken', self.handle_awoken)
+        self.platform = self.config_core.get(
+            "enclosure").get("platform", "unknown")
+        self.wake_word = Configuration.get()['listener']['wake_word']
+
+    @intent_handler(AdaptIntent("NapTimeIntent").require("SleepCommand"))
+    def handle_go_to_sleep(self, _) -> None:
+        """Sends a message to the speech client putting the listener to sleep.
+
+        If the user has been told about the waking up process five times
+        already, it sends a shorter message.
+        """
+        count = self.settings.get('Wake up count', 0)
+        count += 1
+        self.settings['Wake up count'] = count
+
+        if count <= 5:
+            self.speak_dialog('going.to.sleep', {'wake_word': self.wake_word})
+        else:
+            self.speak_dialog('going.to.sleep.short')
+        self.perform_sleep()
+
+    def handle_awoken(self, _) -> None:
+        """Handler for the alice.awoken message
+
+        The message is sent when the listener hears 'Alice, Wake Up',
+        this handles the user interaction upon wake up.
+        """
+        started_by_skill = self.started_by_skill
+
+        self.perform_awaken()
+        if started_by_skill:
+            # Announce that the unit is awake
+            self.speak_dialog("i.am.awake")
+            self.display_waking_face()
+
+    @intent_handler("wake-up.intent")
+    def handle_already_awake(self, _) -> None:
+        """Handle requests to wake up when the system is already awake.
+
+        If the user thought the device was in a sleep mode and tries to wake
+        it up this prevents that going to the Common Query fallbacks.
+        """
+        self.log.info("User tried to wake device, but device was not asleep.")
+        self.speak_dialog('already.awake')
+
+    def display_sleep_face(self) -> None:
+        """Display the sleeping face depending on the platform."""
+        if self.gui.connected:
+            self.gui.show_page("resting.qml", override_idle=True)
+        elif self.platform == "alice_mark_1":
+            self.display_sleep_animation()
+
+    def display_sleep_animation(self) -> None:
+        """Dim and look downward to 'go to sleep'."""
+        # TODO: Get current brightness from somewhere
+        self.old_brightness = 30
+        for i in range(0, (self.old_brightness - 10) // 2):
+            self.enclosure.eyes_brightness(self.old_brightness - i * 2)
+            time.sleep(0.15)
+        self.enclosure.eyes_look("d")
+
+    def display_waking_face(self) -> None:
+        """Display the waking face depending on the platform."""
+        if self.gui.connected:
+            self.gui.remove_page("resting.qml")
+            self.gui.show_page("awake.qml", override_idle=5)
+            # TODO Screen not reverting after the specified 5 seconds.
+            # The following 2 lines shouldn't be needed. Remove when fixed.
+            time.sleep(5)
+            self.gui.release()
+        elif self.platform == 'alice_mark_1':
+            self.display_wake_up_animation()
+
+    def display_wake_up_animation(self) -> None:
+        """Mild animation to come out of sleep from voice command.
+
+        Pop open eyes and wait a sec.
+        """
+        self.enclosure.eyes_reset()
+        time.sleep(1)
+        self.enclosure.eyes_blink('b')
+        time.sleep(1)
+        # brighten the rest of the way
+        self.enclosure.eyes_brightness(self.old_brightness)
+
+    def perform_awaken(self) -> None:
+        """Perform actions to wake system up."""
+        if self.platform != "unknown":
+            self.bus.emit(Message('alice.volume.unmute',
+                                  data={"speak_message": False}))
+        elif self.disabled_confirm_listening:
+            self.enable_confirm_listening()
+
+        self.sleeping = False
+        self.started_by_skill = False
+
+    def perform_sleep(self) -> None:
+        """Perform actions to put system to sleep."""
+        self.bus.emit(Message('recognizer_loop:sleep'))
+        self.sleeping = True
+        self.started_by_skill = True
+        # TODO - Work out why this is here...
+        wait_while_speaking()
+        time.sleep(2)
+        wait_while_speaking()
+        self.display_sleep_face()
+        if self.platform != "unknown":
+            self.bus.emit(Message('alice.volume.mute',
+                                  data={"speak_message": False}))
+        elif self.config_core['confirm_listening']:
+            self.disable_confirm_listening()
+
+    def disable_confirm_listening(self) -> None:
+        """Patch active alice configuration to disable listening beep."""
+        msg = Message('configuration.patch',
+                      data={'config': {'confirm_listening': False}}
+                      )
+        self.bus.emit(msg)
+        self.disabled_confirm_listening = True
+        self.log.info('Disabled listening chirp')
+
+    def enable_confirm_listening(self) -> None:
+        """Patch active alice configuration to enable listening beep."""
+        msg = Message('configuration.patch',
+                      data={'config': {'confirm_listening': True}}
+                      )
+        self.bus.emit(msg)
+        self.disabled_confirm_listening = False
+        self.log.info('Enabled listening chirp again')
+
+
+def create_skill():
+    return NapTimeSkill()
